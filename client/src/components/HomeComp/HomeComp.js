fbf1f8a04ecf18f02beda7d31340f2a107637b57
Anthony Parrino <anthony21parrino21@gmail.com>
2022-02-28 16:01:25 -0500

    created header and working on icons

--- client/src/components/HomeComp/HomeComp.js
+++ client/src/components/HomeComp/HomeComp.js
@@ -1,10 +1,59 @@
 import React from 'react';
 import './homeComp.css';
 
+import HealthInsurance from './health-insurance.png';
+import HealthCare from './healthcare.png';
+
+import AIG from './AIG-logo.png';
+import Globe from './GlobeLife-logo.png';
+import LFG from './LFG-logo.png';
+import LGWhite from './LG-white-logo.png';
+import MoO from './MoO_Horiz_white_rev_color.png';
+import PacificLife from './PacificLife-logo-300x56-1.png';
+import Protective from './Protective-logo.png';
+import Prudential from './Prudential-logo.png';
+
 const HomeComp = () => {
   return (
     <div>
-        home
+      <header className='home-header'>
+        <div className='home-header-info'>
+          <p >You're just a few steps away from finding out how much you could save.</p>
+          <h1>OVER TWO MILLION FAMILIES TRUST ARIELQUOTE</h1>
+          <h5>Life Insurance Starting at Under $1 a day.</h5>
+          <button className='free-quote-btn'>Get Your Free Quote</button>
+        </div>
+      </header>
+      <div className='home-icons'>
+        <section>
+          <div className='card'>
+            <img src={AIG} alt="" />
+          </div>
+          <div className="card">
+            <img src={Globe} alt="" />
+          </div>
+          <div className="card">
+            <img src={LFG} alt="" />
+          </div>
+          <div className="card">
+            <img src={LGWhite} alt="" />
+          </div>
+        </section>
+        <section>
+          <div className='card'>
+            <img src={MoO} alt="" />
+          </div>
+          <div className="card">
+            <img src={PacificLife} alt="" />
+          </div>
+          <div className="card">
+            <img src={Protective} alt="" />
+          </div>
+          <div className="card">
+            <img src={Prudential} alt="" />
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
