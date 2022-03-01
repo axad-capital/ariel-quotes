84bb626b2412ff7b0dfefc25f5c13ab617e6267e
Anthony Parrino <anthony21parrino21@gmail.com>
2022-03-01 12:38:11 -0500

    added footer component

---
+++ client/src/components/GetQuoteComp/GetQuoteComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './getQuoteComp.css';
+
+const GetQuoteComp = () => {
+  return (
+    <div>
+        get a quote
+    </div>
+  )
+}
+
+export default GetQuoteComp
