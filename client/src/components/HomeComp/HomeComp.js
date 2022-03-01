e0febfe9585e7c882574183bc8b5cb3bbf7e2670
Anthony Parrino <anthony21parrino21@gmail.com>
2022-02-28 21:07:50 -0500

    created insurance quote buttons

--- client/src/components/HomeComp/HomeComp.js
+++ client/src/components/HomeComp/HomeComp.js
@@ -16,6 +16,7 @@ import Prudential from './Prudential-logo.png';
 const HomeComp = () => {
   return (
     <div>
+
       <header className='home-header'>
         <div className='home-header-info'>
           <p >You're just a few steps away from finding out how much you could save.</p>
@@ -24,36 +25,60 @@ const HomeComp = () => {
           <button className='free-quote-btn'>Get Your Free Quote</button>
         </div>
       </header>
+
       <div className='home-icons'>
         <section>
-          <div className='card'>
-            <img src={AIG} alt="" />
+          <div className='card card-icons'>
+            <img className='icons' src={AIG} alt="icon" />
           </div>
-          <div className="card">
-            <img src={GlobeLife} alt="" />
+          <div className="card card-icons">
+            <img className='icons' src={GlobeLife} alt="icon" />
           </div>
-          <div className="card">
-            <img src={LFG} alt="" />
+          <div className="card card-icons">
+            <img className='icons' src={LFG} alt="icon" />
           </div>
-          <div className="card">
-            <img src={LGWhite} alt="" />
+          <div className="card card-icons">
+            <img className='icons' src={LGWhite} alt="icon" />
           </div>
         </section>
         <section>
-          <div className='card'>
-            <img src={MoO} alt="" />
+          <div className='card card-icons'>
+            <img className='icons' src={MoO} alt="icon" />
           </div>
-          <div className="card">
-            <img src={PacificLife} alt="" />
+          <div className="card card-icons">
+            <img className='icons' src={PacificLife} alt="icon" />
           </div>
-          <div className="card">
-            <img src={Protective} alt="" />
+          <div className="card card-icons">
+            <img className='icons' src={Protective} alt="icon" />
           </div>
-          <div className="card">
-            <img src={Prudential} alt="" />
+          <div className="card card-icons">
+            <img className='icons' src={Prudential} alt="icon" />
           </div>
         </section>
       </div>
+
+      <div className='we-can-help'>
+        <h2>WE CAN HELP YOU WITH ALL OF YOUR INSURANCE NEEDS</h2>
+        <p>In just minutes, we shop highly rated carriers to find you the best rate... for free!</p>
+        <section className='we-can-help-cards'>
+          <div className='card'>
+            <img src={HealthCare} alt="health care" />
+            <br/>
+            <p>Life Insurance</p>
+          </div>
+          <div className='card'>
+            <img src={HealthInsurance} alt="health insurance" />
+            <br/>
+            <p>Medicare Insurance</p>
+          </div>
+        </section>
+      </div>
+
+      <div>
+        {/* next photo link */}
+        {/* https://www.arielquotes.com/wp-content/uploads/2021/09/FOOTERBG.jpg */}
+      </div>
+
     </div>
   )
 }
