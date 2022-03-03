329e6b0c96860b1556b77273be76ae7ab10d9c46
Anthony Parrino <anthony21parrino21@gmail.com>
2022-03-03 18:30:17 -0500

    working on med overview page and made most pages mobile responsive

--- client/src/components/MedicareComp/MedOverView.js
+++ client/src/components/MedicareComp/MedOverView.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import './medicareComp.css';
+import LGLogo from './LG-logo.png';
+import ALogo from './A-logo.png';
+import TrueLogo from './tru-logo.png';
+import Clip from './clipb.png'
+import Umbrella from './umbrella.png'
 
 const MedOverView = () => {
   return (
     <div>
-        med overview
+      <header className='med-header'>
+        <div className='med-header-info'>
+          <h1>Find Affordable Medicare Plans In Your Area.</h1>
+          <p>$0 monthly premium Medicare Advantage plans may be available in your state.</p>
+          <button onClick={() => window.location.href = '/medicare-quote'} className='free-quote-btn'>Get Your Free Quote</button>
+        </div>
+      </header>
+
+      <div className='med-logo-container'>
+        <img src={ALogo} alt="A logo" />
+        <img src={TrueLogo} alt="true logo" />
+        <img src={LGLogo} alt="lg Logo" />
+      </div>
+
+      <div className='med-sup-adv'>
+        <div className='card med-sup-adv-info'>
+          <h6>Get A Free Quote</h6>
+          <img src={Umbrella} alt="umbrella" />
+          <h4>Medicare Supplement Insurance</h4>
+          <p>Original Medicare covers the majority of medical expenses, but not everything. A Medicare Supplement Insurance plan can help pay for the out-of-pocket costs not covered by Medicare, such as copays, deductibles and coinsurance.</p>
+        </div>
+        <div className='card med-sup-adv-info'>
+          <h6>Get A Free Quote</h6>
+          <img src={Clip} alt='clip'></img>
+          <h4>Medicare Advantage Insurance</h4>
+          <p>Medicare Advantage (MA) plans provide an alternative to Original Medicare and are offered by private health insurance companies approved by Medicare. With MA, you may be able to get $0 premium plan and $0 deductible as well as having prescription drug coverage and other benefits related to vision, dental and hearing.</p>
+        </div>
+      </div>
+
     </div>
   )
 }
