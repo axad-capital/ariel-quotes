9ba4bbcfb398c81fe75cfbbb12f3d00ac825201a
Anthony Parrino <anthony21parrino21@gmail.com>
2022-03-03 19:27:42 -0500

    test

--- client/src/components/MedicareComp/MedOverView.js
+++ client/src/components/MedicareComp/MedOverView.js
@@ -1,29 +1,29 @@
 import React from 'react';
-// import './medicareComp.css';
-// import LGLogo from './LG-logo.png';
-// import ALogo from './A-logo.png';
-// import TrueLogo from './tru-logo.png';
-// import Clip from './clipb.png'
-// import Umbrella from './umbrella.png'
+import './medicareComp.css';
+import LGLogo from './LG-logo.png';
+import ALogo from './A-logo.png';
+import TrueLogo from './tru-logo.png';
+import Clip from './clipb.png'
+import Umbrella from './umbrella.png'
 
 const MedOverView = () => {
   return (
     <div>
-      {/* <header className='med-header'>
+      <header className='med-header'>
         <div className='med-header-info'>
           <h1>Find Affordable Medicare Plans In Your Area.</h1>
           <p>$0 monthly premium Medicare Advantage plans may be available in your state.</p>
           <button onClick={() => window.location.href = '/medicare-quote'} className='free-quote-btn'>Get Your Free Quote</button>
         </div>
-      </header> */}
+      </header>
 
-      {/* <div className='med-logo-container'>
+      <div className='med-logo-container'>
         <img src={ALogo} alt="A logo" />
         <img src={TrueLogo} alt="true logo" />
         <img src={LGLogo} alt="lg Logo" />
-      </div> */}
+      </div>
 
-      {/* <div className='med-sup-adv'>
+      <div className='med-sup-adv'>
         <div className='card med-sup-adv-info'>
           <h6>Get A Free Quote</h6>
           <img src={Umbrella} alt="umbrella" />
@@ -36,7 +36,7 @@ const MedOverView = () => {
           <h4>Medicare Advantage Insurance</h4>
           <p>Medicare Advantage (MA) plans provide an alternative to Original Medicare and are offered by private health insurance companies approved by Medicare. With MA, you may be able to get $0 premium plan and $0 deductible as well as having prescription drug coverage and other benefits related to vision, dental and hearing.</p>
         </div>
-      </div> */}
+      </div>
 
     </div>
   )
